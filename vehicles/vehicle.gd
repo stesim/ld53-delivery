f2f804aefa891bfeb33289720653440dc1e99e75
stesim <simeonov.stefan@gmail.com>
2023-04-29 18:46:00 +0200

    added drivable vehicle

---
+++ vehicles/vehicle.gd
@@ -0,0 +1,12 @@
+extends VehicleBody3D
+
+
+@export_range(0.0, 100.0) var max_engine_force := 50.0
+@export_range(0.0, 100.0) var max_brake_force := 1.0
+@export_range(0.0, 90.0, 0.01, "radians") var max_steering_angle := 0.25 * PI
+
+
+func _physics_process(_delta : float) -> void:
+	engine_force = max_engine_force * Input.get_axis("reverse", "accelerate")
+	brake = max_brake_force * Input.get_action_strength("brake")
+	steering = max_steering_angle * Input.get_axis("steer_right", "steer_left")
