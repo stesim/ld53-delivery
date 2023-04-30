ac9033b258b30d0be5ea67bf43772e6eff819ec5
stesim <simeonov.stefan@gmail.com>
2023-04-30 04:52:00 +0200

    added sounds

--- game_world.gd
+++ game_world.gd
@@ -2,6 +2,7 @@ extends Node3D
 
 
 @onready var _revenue_label := %revenue_label
+@onready var _score_sound := %score_sound
 
 
 func _ready() -> void:
@@ -10,3 +11,5 @@ func _ready() -> void:
 
 func _on_score_changed() -> void:
 	_revenue_label.text = "$%d" % GameState.score
+	_score_sound.stop()
+	_score_sound.play()
--- vehicles/vehicle.gd
+++ vehicles/vehicle.gd
@@ -4,9 +4,11 @@ extends VehicleBody3D
 @export_range(0.0, 500.0, 0.01, "or_greater") var max_engine_force := 50.0
 @export_range(0.0, 100.0) var max_brake_force := 1.0
 @export_range(0.0, 90.0, 0.01, "radians") var max_steering_angle := 0.25 * PI
-@export_range(0.0, 20.0, 0.01, "or_greater") var inventory_loss_acceleration_threshold := 50.0
+@export_range(0.0, 200.0, 0.01, "or_greater") var inventory_loss_acceleration_threshold := 50.0
 @export_range(0.0, 20.0, 0.01, "or_greater") var inventory_loss_amount := 10
 @export_range(0.0, 20.0, 0.01, "or_greater") var inventory_loss_cooldown := 1.0
+@export_range(0.0, 20.0, 0.01, "or_greater") var crash_sound_speed_threshold := 5.0
+
 
 
 var _previous_speed := 0.0
@@ -14,6 +16,7 @@ var _previous_loss_time := 0
 
 
 @onready var _inventory_area := %inventory
+@onready var _crash_sound := %crash_sound
 
 
 func _enter_tree() -> void:
@@ -34,3 +37,8 @@ func _physics_process(delta : float) -> void:
 			_inventory_area.inventory.remove(inventory_loss_amount)
 			_previous_loss_time = now
 	_previous_speed = current_speed
+
+
+func _on_body_entered(_body : Node) -> void:
+	if _previous_speed >= crash_sound_speed_threshold:
+		_crash_sound.play()
