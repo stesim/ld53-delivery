45844b9a1ca29673c4a2267a45fcfb9524b37ac0
stesim <simeonov.stefan@gmail.com>
2023-04-30 15:48:26 +0200

    added background jingles

--- game_world.gd
+++ game_world.gd
@@ -1,12 +1,20 @@
 extends Node3D
 
 
+@export var background_tracks : Array[AudioStream] = []
+
+
 @onready var _revenue_label := %revenue_label
 @onready var _score_sound := %score_sound
 
 
 func _ready() -> void:
 	GameState.score_changed.connect(_on_score_changed)
+	
+	if not background_tracks.is_empty():
+		var background_music = %background_music
+		background_music.stream = background_tracks.pick_random()
+		background_music.play()
 
 
 func _on_score_changed() -> void:
