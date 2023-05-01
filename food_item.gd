2c61cbde7b30f0525e2063832692dd5b9b1ae7ec
stesim <simeonov.stefan@gmail.com>
2023-05-01 02:10:18 +0200

    added spawning of all food items

--- food_item.gd
+++ food_item.gd
@@ -2,3 +2,9 @@ extends RigidBody3D
 
 
 @export var item_scene : PackedScene
+
+
+func _ready() -> void:
+	var item_instance := item_scene.instantiate()
+	item_instance.scale = 0.5 * Vector3.ONE
+	add_child(item_instance)
