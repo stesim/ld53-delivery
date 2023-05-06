3edcb128888e143a25a1a7703cd797d6f75587fd
stesim <simeonov.stefan@gmail.com>
2023-05-06 02:01:02 +0200

    fixed crash sound not playing

--- vehicles/vehicle.gd
+++ vehicles/vehicle.gd
@@ -20,13 +20,12 @@ extends VehicleBody3D
 @export_range(0.0, 100.0) var max_brake_force := 1.0
 @export_range(0.0, 90.0, 0.01, "radians") var max_steering_angle := 0.25 * PI
 @export_range(0.0, 200.0, 0.01, "or_greater") var inventory_loss_acceleration_threshold := 50.0
-@export_range(0.0, 20.0, 0.01, "or_greater") var inventory_loss_amount := 1
-@export_range(0.0, 20.0, 0.01, "or_greater") var inventory_loss_cooldown := 1.0
 @export_range(0.0, 20.0, 0.01, "or_greater") var crash_sound_speed_threshold := 5.0
 @export_range(0.0, 20.0, 0.01, "or_greater") var brake_sound_speed_threshold := 3.0
 
 
 var move_forward := false
+var _previous_speed := 0.0
 
 
 @onready var _crash_sound := %crash_sound
@@ -65,6 +64,8 @@ func _physics_process(_delta : float) -> void:
 	_sound_engine.pitch_scale = 0.5 + speed / 10.0
 	_sound_engine.volume_db = -8.0 + 10.0 * speed / 10.0
 
+	_previous_speed = speed
+
 
 func _unhandled_input(event : InputEvent) -> void:
 	if event.is_action_pressed(&"reset_vehicle"):
@@ -81,5 +82,5 @@ func _reset() -> void:
 func _on_body_entered(body : Node) -> void:
 	if not active or body is RigidBody3D:
 		return
-	if linear_velocity.length() >= crash_sound_speed_threshold:
+	if _previous_speed >= crash_sound_speed_threshold:
 		_crash_sound.play()
