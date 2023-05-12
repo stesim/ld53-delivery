73cb523bb6e396965b4c0c8e4aae8804f9dcf48d
stesim <simeonov.stefan@gmail.com>
2023-05-12 18:03:11 +0200

    testing AStar-based navigation with tile-map map generation

--- maps/tile_map_test_map.gd
+++ maps/tile_map_test_map.gd
@@ -2,10 +2,16 @@
 extends Node3D
 
 
+const GROUND_LAYER := 0
+const BUILDING_LAYER := 1
+const POI_LAYER := 2
+
 const ROAD_TERRAIN_SET_INDEX := 0
 const ROAD_TERRAIN_INDEX := 0
+
 const SCENE_LAYER_INDEX := 0
 const SCENE_ROTATION_LAYER_INDEX := 1
+const WALKABLE_LAYER_INDEX := 2
 
 
 @export var tile_size_coarse := 6.0 * Vector3.ONE
@@ -21,6 +27,9 @@ var _container_coarse_tiles : Node3D = null
 var _container_regular_tiles : Node3D = null
 var _container_building_tiles : Node3D = null
 
+var _astar := AStarGrid2D.new()
+var _astar_region := Rect2i()
+
 
 @onready var _tile_map_coarse : TileMap = $tile_map_coarse
 @onready var _tile_map_regular : TileMap = $tile_map_regular
@@ -31,25 +40,53 @@ func _ready() -> void:
 		_tile_map_coarse.hide()
 		_tile_map_regular.hide()
 
+	_astar.jumping_enabled = true
+	_astar.diagonal_mode = AStarGrid2D.DIAGONAL_MODE_ONLY_IF_NO_OBSTACLES
+
 	_update()
+
+	if not Engine.is_editor_hint():
+		_test_navigation()
+
+
+func _test_navigation() -> void:
+	var start_position := 0.5 * tile_size_regular
+	var start_id := _world_position_to_astar_cell_id(start_position)
+
+	var poi_cells := _tile_map_regular.get_used_cells(POI_LAYER)
+	var target_id := _tile_map_index_to_astar_cell_id(poi_cells.pick_random())
+
+	var id_path := _astar.get_id_path(start_id, target_id)
+
+	var navigation_path : Path3D = $navigation_path
+	for id in id_path:
+		navigation_path.curve.add_point(_astar_cell_id_to_world_position(id))
+
+	var path_follower : PathFollow3D = navigation_path.get_child(0)
+	create_tween().tween_property(
+		path_follower,
+		"progress",
+		navigation_path.curve.get_baked_length(),
+		0.5 * navigation_path.curve.get_baked_length(),
+	)
 
 
 func _update() -> void:
+	var coarse_tiles_offset := Vector3(0.5 * tile_size_coarse.x, 0.0, 0.5 * tile_size_coarse.y)
 	_container_coarse_tiles = _recreate_container(&"_generated_tiles_coarse")
-	_generate_scenes_from_tiles(_tile_map_coarse, tile_size_coarse, _container_coarse_tiles, 0)
+	_container_coarse_tiles.position = coarse_tiles_offset
+	_generate_scenes_from_tiles(_tile_map_coarse, tile_size_coarse, _container_coarse_tiles, GROUND_LAYER)
 
-	var regular_tiles_offset := Vector3(
-		-0.5 * (tile_size_coarse.x - tile_size_regular.x),
-		0.0,
-		-0.5 * (tile_size_coarse.y - tile_size_regular.y),
-	)
+	var regular_tiles_offset := Vector3(0.5 * tile_size_regular.x, 0.0, 0.5 * tile_size_regular.y)
 	_container_regular_tiles = _recreate_container(&"_generated_tiles_regular")
 	_container_regular_tiles.position = regular_tiles_offset
-	_generate_scenes_from_tiles(_tile_map_regular, tile_size_regular, _container_regular_tiles, 0)
+	_generate_scenes_from_tiles(_tile_map_regular, tile_size_regular, _container_regular_tiles, GROUND_LAYER)
 
 	_container_building_tiles = _recreate_container(&"_generated_tiles_buildings")
 	_container_building_tiles.position = regular_tiles_offset + building_ground_height * Vector3.UP
-	_generate_scenes_from_tiles(_tile_map_regular, tile_size_regular, _container_building_tiles, 1)
+	_generate_scenes_from_tiles(_tile_map_regular, tile_size_regular, _container_building_tiles, BUILDING_LAYER)
+
+	_setup_navigation()
 
 
 func _generate_scenes_from_tiles(tile_map : TileMap, tile_size : Vector3, container : Node, layer : int) -> void:
@@ -68,13 +105,46 @@ func _generate_scenes_from_tiles(tile_map : TileMap, tile_size : Vector3, contai
 			container.add_child(tile_instance)
 
 
-#func _get_tile_source(tile_set : TileSet, source_name : String) -> TileSetSource:
-#	for i in tile_set.get_source_count():
-#		var id := tile_set.get_source_id(i)
-#		var source := tile_set.get_source(id)
-#		if source.resource_name == source_name:
-#			return source
-#	return null
+func _setup_navigation() -> void:
+	_astar_region = _tile_map_regular.get_used_rect()
+
+	_astar.clear()
+	_astar.size = _astar_region.size
+	_astar.update()
+
+	for y in _astar_region.size.y:
+		for x in _astar_region.size.x:
+			var id := Vector2i(x, y)
+			var tile_map_coords := _astar_region.position + id
+			var ground_data := _tile_map_regular.get_cell_tile_data(GROUND_LAYER, tile_map_coords)
+			var is_walkable : bool = ground_data != null and ground_data.get_custom_data_by_layer_id(WALKABLE_LAYER_INDEX)
+			if is_walkable:
+				var is_building := _tile_map_regular.get_cell_tile_data(BUILDING_LAYER, tile_map_coords) != null
+				is_walkable = is_walkable and not is_building
+
+			if not is_walkable:
+				_astar.set_point_solid(id)
+
+
+func _world_position_to_astar_cell_id(world_position : Vector3) -> Vector2i:
+	var tiles_local_position := _container_regular_tiles.to_local(world_position)
+	var fractional_grid_position := tiles_local_position / tile_size_regular
+	var tile_map_index := Vector2i(
+		floori(fractional_grid_position.x),
+		floori(fractional_grid_position.z),
+	)
+	var astar_cell_id := _tile_map_index_to_astar_cell_id(tile_map_index)
+	return astar_cell_id
+
+
+func _astar_cell_id_to_world_position(id : Vector2i) -> Vector3:
+	var tile_map_index := _astar_region.position + id
+	var tile_map_position := Vector3(tile_map_index.x, 0.0, tile_map_index.y) * tile_size_regular
+	return _container_regular_tiles.to_global(tile_map_position)
+
+
+func _tile_map_index_to_astar_cell_id(index : Vector2i) -> Vector2i:
+	return index - _astar_region.position
 
 
 func _recreate_container(node_name : String) -> Node3D:
